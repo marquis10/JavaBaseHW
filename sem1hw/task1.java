251ba19fc08e73f0053a27ca3ec374fd0d48e3e9
“Alexandr <kuzmin_89@mail.ru>
2023-06-24 22:59:08 +0500

    1task

---
+++ sem1hw/task1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class task1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите число N: ");
+        int N = iScanner.nextInt();
+        iScanner.close();
+        int sumNum = 0, sumFac = 1;
+        for (int i = 1; i < N + 1; i++) {
+            sumNum = sumNum + i;
+            sumFac = sumFac * i;
+        }
+        System.out.printf("Число N = %d", N);
+        System.out.printf("\nСумма чисел от 1 до N = %d", sumNum);
+        System.out.printf("\nФакториал числа N = %d", sumFac);
+    }
+}
