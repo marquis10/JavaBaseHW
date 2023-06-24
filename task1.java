e81e93bf408f1cb78cd8ffed823701dbe302f407
“Alexandr <kuzmin_89@mail.ru>
2023-06-24 23:01:18 +0500

    task1_last

--- task1.java
+++ task1.java
@@ -1,4 +1,7 @@
+// 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
 import java.util.Scanner;
+
 public class task1 {
     public static void main(String[] args) {
         Scanner iScanner = new Scanner(System.in);
