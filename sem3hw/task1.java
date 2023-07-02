ea589a11338aa4ea45c675b5f359c403671883da
“Alexandr <kuzmin_89@mail.ru>
2023-07-03 00:42:42 +0500

    убрал лишнее

--- sem3hw/task1.java
+++ sem3hw/task1.java
@@ -2,24 +2,18 @@ package sem3hw;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class task1 {
     public static void main(String[] args) {
-        ArrayList<Integer> intList = new ArrayList<Integer>(Arrays.asList(4,2,3,4,4,9,2,5,7,1,8,6,7));
+        ArrayList<Integer> intList = new ArrayList<>(Arrays.asList(4,2,3,4,4,9,2,5,7,1,8,6,7));
+        System.out.println(intList);
         delEven(intList);
         // minValue(intList);
         // maxValue(intList);
         // averageValue(intList);
-        System.out.println(intList);
-        // for (Integer integer : intList) {
-        //     if (integer % 2 == 0) {
-        //         intList.remove(integer);
-        //     }
-        // }
-
     }
+
 
     public static void delEven(ArrayList<Integer> list){
         int count = 1;
@@ -34,13 +28,16 @@ public class task1 {
         System.out.println(list);
     }
 
+
     public static void minValue(ArrayList<Integer> list) {
         
     }
 
+
     public static void maxValue(ArrayList<Integer> list) {
         
     }
+
 
     public static void averageValue(ArrayList<Integer> list) {
         
