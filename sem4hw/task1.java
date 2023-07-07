ed57cba4a4072373925f8cc8d5955794b7d72695
Alexandr Kuzmin <kuzmin_89@mail.ru>
2023-07-07 17:05:41 +0500

    4 семинар 1 задание

---
+++ sem4hw/task1.java
@@ -0,0 +1,24 @@
+package sem4hw;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class task1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> linked = new LinkedList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        reverse(linked);
+    }
+
+    static void reverse(LinkedList<Integer> linked){
+        LinkedList<Integer> temp = new LinkedList<>();
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            
+        }
+            //temp.add(count, listIterator.previous());
+            System.out.println();
+            temp.push();
+        }
+    }
+}
